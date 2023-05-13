42f59f2c53120a7a1e16baf42255bd843925dabe
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2023-05-13 18:39:16 +0200

    [Mod] window title, eslint + format code

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,6 @@ module.exports = {
   rules: {
     "react-refresh/only-export-components": "warn",
     "@typescript-eslint/no-empty-interface": "off",
-    "@typescript-eslint/no-unused-vars": "off"
+    "@typescript-eslint/no-empty-function": "off",
   },
 };
