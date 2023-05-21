56ae41ae6419840c4bdb5e3c9311d8da1b9c0063
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2023-05-21 15:45:34 +0200

    [Add] use case for get user data

--- src/client/pages/home/index.tsx
+++ src/client/pages/home/index.tsx
@@ -2,17 +2,36 @@ import { Props } from "./type.ts";
 import styles from "./styles.module.scss";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import { useGetUser } from "@todo-list/view-models";
+import { useEffect } from "react";
 
 function Home(_: Props) {
   const { t } = useTranslation();
   const navigate = useNavigate();
+  const {
+    user,
+    getUser,
+    isRequestFailure,
+    isRequestPending,
+    isRequestSuccess,
+  } = useGetUser();
   const goToProfile = () => {
     navigate("/profile");
   };
 
+  useEffect(() => {
+    getUser({
+      accessToken: localStorage.getItem("pad-todolist-accessToken") ?? "",
+      id: localStorage.getItem("pad-todolist-userId") ?? "",
+    });
+  }, []);
+
   return (
-    <div className={styles.login}>
+    <div className={styles.home}>
       <p>{t("homePage.title")}</p>
+      {isRequestSuccess && <div>{user.nickName}</div>}
+      {isRequestFailure && <div>{isRequestFailure.message}</div>}
+      {isRequestPending && <div>loading...</div>}
       <button onClick={goToProfile}>profile</button>
     </div>
   );
--- src/core/src/viewModels/main.ts
+++ src/core/src/viewModels/main.ts
@@ -1,3 +1,4 @@
 export * from "./checkApi.ts";
 export * from "./register.ts";
 export * from "./login.ts";
+export * from "./getUser.ts";
