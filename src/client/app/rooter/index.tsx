745f0fc50f988e1a17fd0d5f37a79274c328450e
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2023-05-24 20:31:51 +0200

    [Mod] update proxy
    use env var for server url

--- src/client/app/rooter/index.tsx
+++ src/client/app/rooter/index.tsx
@@ -8,7 +8,6 @@ function Rooter() {
   const userId = localStorage.getItem("pad-todolist-userId");
   const userToken = localStorage.getItem("pad-todolist-userToken");
 
-  // Update the isAuthenticated state based on the presence of userId and userToken
   useEffect(() => {
     if (userId && userToken) {
       setIsAuthenticated(true);
